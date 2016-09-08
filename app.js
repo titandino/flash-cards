979f3ab0d7764078dc914923c2dea03e1fc05b46
Trenton Kress <trenton.kress@gmail.com>
2016-09-07 19:41:02 -0700

    Renamed object to flash card

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function Card(question, answers) {
+function FlashCard(question, answers) {
   this.question = question;
   this.answers = answers;
 }
 
-Card.prototype.checkAnswer = function(answer) {
+FlashCard.prototype.checkAnswer = function(answer) {
   for (possible in this.answers) {
     if (answer.toLowerCase() === possible.toLowerCase())
       return true;
