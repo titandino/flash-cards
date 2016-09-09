cd19a4662fd167cae62f93395e4f19fba0c93ebb
Trenton Kress <trenton.kress@gmail.com>
2016-09-08 19:37:54 -0700

    Started on the input, update, and rendering of the cards.

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
+var inputHandler = {
+  keysDown = [],
+  mousePos: {x: 0, y: 0},
+
+  getMousePos: function(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+      y: (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+    };
+  },
+}
+
 var kinderKards = {
   canvas: document.getElementById('card-canvas'),
-  ctx: canvas.getContext('2d'),
+  ctx: canvas.getContext('2d');
+
+  currentCard: new BasicCard('What is love?', 'baby don\'t hurt me');
+
+  drawText: function(text, x, y, color, size, font, align) {
+    this.ctx.fillStyle = color;
+    this.ctx.font = (size || '12px') + ' ' + (font || 'Helvetica');
+    this.ctx.textAlign = align || 'left';
+    this.ctx.textBaseline = 'top';
+    this.ctx.fillText(text, x, y);
+  },
 
   getRandom: function(min, max) {
     return Math.floor(Math.random() * (max-min) + min);
   },
+
+  update: function(delta) {
+    kinderKards.currentCard.update(delta);
+  },
+
+  render: function() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    kinderKards.currentCard.render();
+  },
+
+  then: Date.now(),
+
+  main: function() {
+    var now = Date.now();
+    var delta = now - this.then;
+
+    update(delta / 1000);
+    render();
+
+    this.then = now;
+  }
 }
 
 function BasicCard(question, answer) {
@@ -18,6 +62,16 @@ BasicCard.prototype.checkAnswer = function(answer) {
   return false;
 };
 
+BasicCard.prototype.update = function(delta) {
+
+};
+
+BasicCard.prototype.render = function() {
+
+};
+
+CountCard.prototype = Object.create(BasicCard.prototype);
+
 function CountCard(question, image, min, max) {
   BasicCard.call(this, question);
   this.image = new Image();
@@ -29,3 +83,33 @@ function CardCategory(name, cards) {
   this.name = name;
   this.cards = cards;
 }
+
+kinderKards.canvas.addEventListener('mousedown', function(e) {
+  mousePos = inputHandler.getMousePos(canvas, e);
+  //TODO current card click callback
+}, false);
+
+kinderKards.canvas.addEventListener('mousemove', function(e) {
+  mousePos = inputHandler.getMousePos(canvas, e);
+}, false);
+
+window.addEventListener('keydown', function(e) {
+  inputHandler.keysDown[e.keyCode] = true;
+  switch(e.keyCode){
+  case 37:
+  case 39:
+  case 38:
+  case 40:
+  case 32:
+    e.preventDefault();
+    break;
+  default:
+    break;
+  }
+}, false);
+
+window.addEventListener('keyup', function(e) {
+  inputHandler.keysDown[e.keyCode] = false;
+}, false);
+
+setInterval(kinderKards.main, 16.666667);
