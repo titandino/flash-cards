f875498e293414f93d58652058590d4a16b60b72
Trenton Kress <trenton.kress@gmail.com>
2016-09-08 18:27:39 -0700

    Basic layout for the number cards

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-function FlashCard(question, answers) {
+function BasicCard(question, answer) {
   this.question = question;
-  this.answers = answers;
+  this.answer = answer;
 }
 
-FlashCard.prototype.checkAnswer = function(answer) {
-  for (possible in this.answers) {
-    if (answer.toLowerCase() === possible.toLowerCase())
-      return true;
-  }
+BasicCard.prototype.checkAnswer = function(answer) {
+  if (this.answer === answer)
+    return true;
   return false;
 };
+
+function CountCard(question, image, min, max) {
+  BasicCard.call(this, question);
+  this.image = new Image();
+  this.image.src = image;
+  this.answer = getRandom(min, max);
+}
+
+function CardCategory(name, cards) {
+  this.name = name;
+  this.cards = cards;
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max-min) + min);
+}
+
+var kinderKards = {
+  category1: new CardCategory('Numbers', [
+    new FlashCard('')
+  ]);
+}
