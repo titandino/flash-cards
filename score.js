0ef60ec2174792749f9ead1feacea51ef00f313f
Trenton Kress <trenton.kress@gmail.com>
2016-09-18 09:46:49 -0700

    Fixed up the scores page to be more child friendly.

--- score.js
+++ score.js
@@ -5,28 +5,16 @@ var scoring = {
     parent.appendChild(ele);
   },
 
-  createTable: function() {
-    var table = document.getElementById('scoresTable');
-    var header = document.createElement('tr');
-    this.appendNewElement(header, 'th', 'Category');
-    this.appendNewElement(header, 'th', 'Score');
-    table.appendChild(header);
-  },
-
   appendScoreData: function() {
-    var table = document.getElementById('scoresTable');
-    var row = document.createElement('tr');
+    var section = document.getElementById('scoreSection');
     var scores = JSON.parse(localStorage.scores);
     for (i in scores) {
-      this.appendNewElement(row, 'td', scores[i].name + ':');
-      this.appendNewElement(row, 'td', scores[i].score);
+      this.appendNewElement(section, 'p', 'You scored a ' + scores[i].score + ' in the ' + scores[i].name + ' category!');
     }
-    table.appendChild(row);
   },
 
   create: function() {
     if (localStorage.scores) {
-      this.createTable();
       this.appendScoreData();
     } else {
       this.appendNewElement(document.getElementById('mainBody'), 'h3', 'No scores found. Go earn some scores!');
